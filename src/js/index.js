1193e59003f8e5475f7863c783d30a8d5167db8f
Guilherme de Favere <guilherme.favere@gmail.com>
2019-10-18 23:04:55 -0300

    Create method to search recipes

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
-// global app controller
-import num from './test'
+import axios from 'axios'
 
-const x = 23
+async function getResults(query) {
+    const key = 'b2b0debb84bed3887b3013a454442d65'
 
-console.log(`I imported ${num} from another module! Variable x is ${x}`)
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes
+
+        console.log(recipes)
+    } catch (error) {
+        alert(error)
+    }
+}
+
+getResults('tomato pasta')
