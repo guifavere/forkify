39953502853533aebca2c00e596c157513617a3b
Guilherme de Favere <guilherme.favere@gmail.com>
2019-10-19 17:40:48 -0300

    Create the Search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,3 @@
-import axios from 'axios'
+import Search from './models/Search'
 
-async function getResults(query) {
-    const key = 'b2b0debb84bed3887b3013a454442d65'
-
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
-        const recipes = res.data.recipes
-
-        console.log(recipes)
-    } catch (error) {
-        alert(error)
-    }
-}
-
-getResults('tomato pasta')
+const search = new Search('pizza')
---
+++ src/js/models/Search.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default class Search {
+    constructor(query) {
+        this.query = query
+    }
+
+    async getResults() {
+        const key = 'b2b0debb84bed3887b3013a454442d65'
+
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            this.result = res.data.recipes
+        } catch (error) {
+            alert(error)
+        }
+    }
+}
