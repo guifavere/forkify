e0f5d17838c1e70c07ab259ca10f83122b862620
Guilherme de Favere <guilherme.favere@gmail.com>
2019-10-31 13:14:10 -0300

    Create shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
+import List from './models/List'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import { elements, renderLoader, clearLoader } from './views/base'
@@ -110,3 +111,5 @@ elements.recipe.addEventListener('click', e => {
         recipeView.updateServingsIngredients(state.recipe)
     }
 })
+
+window.l = new List()
