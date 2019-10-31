f0b3bb382e87b081e5c3e83f11254ed2f4d0f2cf
Guilherme de Favere <guilherme.favere@gmail.com>
2019-10-31 19:27:14 -0300

    Create the likes controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search'
 import Recipe from './models/Recipe'
 import List from './models/List'
+import Likes from './models/Likes'
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
@@ -113,6 +114,40 @@ const controlList = () => {
     })
 }
 
+/**
+ * LIKE CONTROLLER
+ */
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes()
+
+    const currentID = state.recipe.id
+
+    // user has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        // add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        )
+
+        // toggle the like button
+
+        // add like to UI list
+        console.log(state.likes)
+    // user has liked current recipe
+    } else {
+        // remove like to the state
+        state.likes.deleteLike(currentID)
+
+        // toggle the like button
+
+        // remove like from UI list
+        console.log(state.likes)
+    }
+}
+
 // handling delete and update item events
 elements.shopping.addEventListener('click', e => {
     const id = e.target.closest('.shopping__item').dataset.itemid
@@ -145,6 +180,10 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc')
         recipeView.updateServingsIngredients(state.recipe)
     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        // add ingredients to shopping list
         controlList()
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // like control
+        controlLike()
     }
 })
